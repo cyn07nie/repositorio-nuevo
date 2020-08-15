af547ae467064fce7e4e46c7e99b8179f6db3de9
cyn07nie <cynieto0798@gmail.com>
2020-08-15 20:28:45 -0300

    prueba

--- js/products.js
+++ js/products.js
@@ -4,3 +4,42 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+var productsArray = [];
+
+function showProductsList(array) {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let producto = array[i];
+        
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + producto.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ producto.name + `</h4>
+                        <small class="font-muted">` + producto.currency + " $" + producto.cost + `<br> `+ producto.soldCount + ` artículos vendidos </small>                    
+                     </div>
+                     ${producto.description}
+                 </div>
+             </div>
+         </div>
+        `
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
+});//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+
+});
