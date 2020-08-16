ba34af8339ecc492501721b8d2cb74fca978fcac
cyn07nie <cynieto0798@gmail.com>
2020-08-16 15:40:30 -0300

    entrega 1

--- js/products.js
+++ js/products.js
@@ -73,3 +73,13 @@ document.addEventListener("DOMContentLoaded", function(e){
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+const requestURL = "https://japdevdep.github.io/ecommerce-api/product/all.json"
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+  }
